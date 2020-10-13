9bde5618a0c703a173c69ffba6c72592873be660
Nicole004 <72332575+Nicole004@users.noreply.github.com>
2020-10-13 11:13:51 -0600

    creacion de boton y tabla de usuarios

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,10 +1,14 @@
 @extends("layouts.main")
 @section("content")
+
+    <a class="btn btn-primary" href="{{route('/dashboard')}}"> CREAR USUARIO</a>
+
     <img
          class="align-center"
          width="50%"
          style="margin-left: 25%; opacity: 0.8"
          height="50%"
          src="/images/logo/logo.png">
+
 
 @endsection
