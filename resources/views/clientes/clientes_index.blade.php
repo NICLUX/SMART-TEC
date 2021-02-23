1a2e348a6543d95af3129878292dbe22a3413388
LuisCh4con <luischacon1797@gmail.com>
2021-02-23 11:21:54 -0600

    Modificacion de tablas clientes_index.blade.php, proveedores_index.blade.php, y de inventario_index.blade.php, siguiendo con el aspecto de mejoramiento visual

--- resources/views/clientes/clientes_index.blade.php
+++ resources/views/clientes/clientes_index.blade.php
@@ -1,75 +1,84 @@
-@extends("layouts.main")
+@extends('layouts.main')
+@extends('servicios.mejora_vista')
 @section("content")
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Clientes</h3>
+    <div class="btn-group float-right float-left" role="group" aria-label="Basic example" id="botones_ser">
+        <a class="btn btn-secondary float-right" href="{{route("usuarios.index")}}">Vista Tarjetas</a>
+        <a class="btn btn-secondary float-right" href="{{route("usuarios.create")}}">Agregar</a>
+    </div>
+    <br>
+    <hr>
+    <!---Alerta y envia mensajes al cliente cuando hay un error o se registran -->
+    @if(session("exito"))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{session("exito")}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-        <div class="card-body">
-            <a class="btn btn-success btn-sm float-right" href="{{route("cliente.nuevo")}}"><i class="fa fa-plus"></i>
-                Agregar</a>
-            <br>
-            <hr>
+    @endif
+    @if(session("error"))
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <span class="fa fa-save"></span> {{session("error")}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
 
-            <!---Alerta y envia mensajes al cliente cuando hay un error o se registran -->
-            @if(session("exito"))
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{session("exito")}}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
-            @if(session("error"))
-                <div class="alert alert-danger alert-dismissible" role="alert">
-                    <span class="fa fa-save"></span> {{session("error")}}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+        </div>
+    @endif
 
+    <div class="container-fluid" >
+        <div class="panel panel-success" id="encabezado">
+            <div class="panel-heading">
+                <div class="row" id="color_panel">
+                    <div class="col-xs-12 col-sm-12 col-md-3" >
+                        <h2 class="text-center pull-left" style="padding-left: 30px;">
+                            <span class="glyphicon glyphicon-list-alt"> </span>Clientes</h2>
+                    </div>
                 </div>
-            @endif
-            <div class="card card-body">
-                <div class="container-fluid">
-                    @if($clientes->count())
-                    <table class="table">
-                        <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Télefono</th>
-                            <th scope="col">Dirección</th>
-                            <th scope="col">Opciones</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($clientes as $item=>$cliente)
-                            <tr>
-                                <th>{{$item+$clientes->firstItem()}}</th>
-                                <td>{{$cliente->nombre}}</td>
-                                <td>{{$cliente->telefono}}</td>
-                                <td>{{$cliente->direccion}}</td>
-                                <td>
-                                    <a class="btn btn-sm btn-success"
-                                       href="{{route("cliente.editar",["id"=>$cliente->id])}}"
-                                       title="Editar"><i class="fa fa-pencil"></i></a>
-                                    <a class="btn btn-sm btn-danger"
-                                       href="{{route("cliente.destroy",["id"=>$cliente->id])}}"
-                                       title="Eliminar"><i class="fa fa-trash"></i></a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                        <div class="pagination pagination-sm justify-content-center">
-                            {{$clientes->links()}}
+
+                <div class="panel-body table-responsive">
+                    <div class="card card-body">
+                        <div class="container-fluid">
+                            @if($clientes->count())
+                                <table class="table">
+                                    <thead class="table table-hover">
+                                    <tr id="tabla">
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Télefono</th>
+                                        <th scope="col">Dirección</th>
+                                        <th scope="col">Opciones</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($clientes as $item=>$cliente)
+                                        <tr id="resultados">
+                                            <th>{{$item+$clientes->firstItem()}}</th>
+                                            <td>{{$cliente->nombre}}</td>
+                                            <td>{{$cliente->telefono}}</td>
+                                            <td>{{$cliente->direccion}}</td>
+                                            <td>
+                                                <a class="btn btn-sm btn-success"
+                                                   href="{{route("cliente.editar",["id"=>$cliente->id])}}"
+                                                   title="Editar"><i class="fa fa-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"
+                                                   href="{{route("cliente.destroy",["id"=>$cliente->id])}}"
+                                                   title="Eliminar"><i class="fa fa-trash"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                                <div class="pagination pagination-sm justify-content-center">
+                                    {{$clientes->links()}}
+                                </div>
+                            @else
+                                <div class="alert alert-info">
+                                    No hay clientes ingresados aún.
+                                </div>
+                            @endif
                         </div>
-                    @else
-                        <div class="alert alert-info">
-                            No hay clientes ingresados aún.
-                        </div>
-                    @endif
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 @endsection
--- resources/views/proveedores/proveedores_index.blade.php
+++ resources/views/proveedores/proveedores_index.blade.php
@@ -1,91 +1,96 @@
-@extends("layouts.main")
+@extends('layouts.main')
+@extends('servicios.mejora_vista')
 @section("content")
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">Proveedores</h3>
+    <div class="btn-group float-right float-left" role="group" aria-label="Basic example" id="botones_ser">
+        <a class="btn btn-secondary float-right" href="{{route("proveedores.index")}}">Vista Tarjetas</a>
+        <a class="btn btn-secondary float-right" href="{{route("proveedor.nuevo")}}">Agregar</a>
+    </div>
+    <br>
+    <hr>
+    <!---Alerta y envia mensajes al cliente cuando hay un error o se registran -->
+    @if(session("exito"))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{session("exito")}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-        <div class="card-body">
-            <a class="btn btn-success btn-sm float-right" href="{{route("proveedor.nuevo")}}"><i class="fa fa-plus"></i>
-                Agregar</a>
-            <br>
-            <hr>
-            <br>
-            <br>
-            <!---Alerta y envia mensajes al cliente cuando hay un error o se registran -->
-            @if(session("exito"))
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{session("exito")}}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-            @endif
-            @if(session("error"))
-                <div class="alert alert-danger alert-dismissible" role="alert">
-                    <span class="fa fa-save"></span> {{session("error")}}
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+    @endif
+    @if(session("error"))
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <span class="fa fa-save"></span> {{session("error")}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
 
-                </div>
-            @endif
-            <div class="card card-body">
-                <div class="container-fluid">
-                    @if($proveedores->count()>0)
-                    <table class="table">
-                        <thead class="thead-dark">
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Dirección</th>
-                            <th scope="col">Télefono</th>
-                            <th scope="col">Descripción</th>
-                            <th scope="col">Correo</th>
-                            <th scope="col">Opciones</th>
-
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($proveedores as $item=> $proveedor)
-
-                            <tr>
-                                <th scope="col">{{$item+$proveedores->firstItem()}}</th>
-                                <td>{{$proveedor->nombre}}</td>
-                                <td>{{$proveedor->direccion}}</td>
-                                <td>{{$proveedor->telefono}}</td>
-                                @if($proveedor->descripcion)
-                                <td>{{$proveedor->descripcion}}</td>
-                                @else
-                                    <td>n/a</td>
-                                @endif
-                                @if($proveedor->descripcion)
-                                <td>{{$proveedor->correo}}</td>
-                                @else
-                                    <td>n/a</td>
-                                @endif
-                                <td>
-                                    <a class="btn btn-sm btn-success"
-                                       href="{{route("proveedor.editar",["id"=>$proveedor->id])}}"
-                                       title="Editar"><i class="fa fa-pencil"></i></a>
-                                    <a class="btn btn-sm btn-danger"
-                                       href="{{route("proveedor.destroy",["id"=>$proveedor->id])}}"
-                                       title="Eliminar"><i class="fa fa-trash"></i></a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                    <div class="pagination pagination-sm justify-content-center">
-                        {{$proveedores->links()}}
+        </div>
+    @endif
+    <div class="container-fluid" >
+        <div class="panel panel-success" id="encabezado">
+            <div class="panel-heading">
+                <div class="row" id="color_panel">
+                    <div class="col-xs-12 col-sm-12 col-md-3" >
+                        <h2 class="text-center pull-left" style="padding-left: 30px;">
+                            <span class="glyphicon glyphicon-list-alt"> </span>Proveedores</h2>
                     </div>
-                    @else
-                        <div class="alert alert-info">
-                            <h4>No hay proveedores agregador aún, presiona el botón de agregar.</h4>
-                        </div>
-                    @endif
+                </div>
+                <div class="card card-body">
+                    <div class="container-fluid">
+                        @if($proveedores->count()>0)
+                            <table class="table">
+                                <thead class="table table-hover">
+                                <tr id="tabla">
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Dirección</th>
+                                    <th scope="col">Télefono</th>
+                                    <th scope="col">Descripción</th>
+                                    <th scope="col">Correo</th>
+                                    <th scope="col">Opciones</th>
+
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($proveedores as $item=> $proveedor)
+
+                                    <tr id="resultados">
+                                        <th scope="col">{{$item+$proveedores->firstItem()}}</th>
+                                        <td>{{$proveedor->nombre}}</td>
+                                        <td>{{$proveedor->direccion}}</td>
+                                        <td>{{$proveedor->telefono}}</td>
+                                        @if($proveedor->descripcion)
+                                            <td>{{$proveedor->descripcion}}</td>
+                                        @else
+                                            <td>n/a</td>
+                                        @endif
+                                        @if($proveedor->descripcion)
+                                            <td>{{$proveedor->correo}}</td>
+                                        @else
+                                            <td>n/a</td>
+                                        @endif
+                                        <td>
+                                            <a class="btn btn-sm btn-success"
+                                               href="{{route("proveedor.editar",["id"=>$proveedor->id])}}"
+                                               title="Editar"><i class="fa fa-pencil"></i></a>
+                                            <a class="btn btn-sm btn-danger"
+                                               href="{{route("proveedor.destroy",["id"=>$proveedor->id])}}"
+                                               title="Eliminar"><i class="fa fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            <div class="pagination pagination-sm justify-content-center">
+                                {{$proveedores->links()}}
+                            </div>
+                        @else
+                            <div class="alert alert-info">
+                                <h4>No hay proveedores agregador aún, presiona el botón de agregar.</h4>
+                            </div>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 @endsection
