6f7babeab8c6d2c650d25a213ebfc5a988ee2a10
Valdivia <xavier.valdivia@unah.hn>
2021-04-29 00:04:08 -0600

    MODIFICANDO CATEGORIA PRODUCTO

--- resources/views/compras/ProductosCompras.blade.php
+++ resources/views/compras/ProductosCompras.blade.php
@@ -198,6 +198,7 @@
                         </div>
                     @endif
 
+
                     <div class="container register" id="detalle_form_prov">
                         <div class="row" id="detalle_form_prov">
                             <div class="col-md-3 register-left">
@@ -213,80 +214,85 @@
                                         <div class="row register-form">
                                             <div class="col-md-6">
 
-                                         <form id="form_proveedores" style="    width: 450px;" enctype="multipart/form-data"                      action="{{route("proveedor.store")}}" method="post">
-                                            @csrf
-                                            <div class="form-group">
-                                                <label>Ingrese el nombre:</label>
-                                                <input class="form-control  @error('nombre') is-invalid @enderror"
-                                                       placeholder="Nombre"
-                                                       required
-                                                       value="{{old("nombre")}}"
-                                                       maxlength="80" name="nombre">
-                                                @error('nombre')
-                                                <span class="invalid-feedback" role="alert">
+                                                <form id="form_proveedores" style="    width: 450px;" enctype="multipart/form-data"                 action="{{route("proveedor.stor")}}"
+                                                       method="post">
+                                                    @csrf
+                                                    <div class="form-group">
+                                                        <label>Ingrese el nombre:</label>
+                                                        <input class="form-control  @error('nombre') is-invalid @enderror"
+                                                               placeholder="Nombre"
+                                                               required
+                                                               value="{{old("nombre")}}"
+                                                               maxlength="80" name="nombre">
+                                                        @error('nombre')
+                                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Ingrese la descripción (opcional):</label>
-                                                <textarea class="form-control @error('descripcion') is-invalid @enderror"
-                                                          placeholder="Direccion exacta"
-                                                          maxlength="80" name="descripcion">{{old("descripcion")}}</textarea>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Ingrese la descripción (opcional):</label>
+                                                        <textarea class="form-control @error('descripcion') is-invalid @enderror"
+                                                                  placeholder="Direccion exacta"
+                                                                  maxlength="80" name="descripcion">{{old("descripcion")}}</textarea>
 
-                                                @error('descripcion')
-                                                <span class="invalid-feedback" role="alert">
+                                                        @error('descripcion')
+                                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Ingrese la direccion:</label>
-                                                <textarea class="form-control @error('direccion') is-invalid @enderror"
-                                                          placeholder="Direccion exacta"
-                                                          required
-                                                          maxlength="80" name="direccion">{{old("direccion")}}</textarea>
-                                                @error('direccion')
-                                                <span class="invalid-feedback" role="alert">
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Ingrese la direccion:</label>
+                                                        <textarea class="form-control @error('direccion') is-invalid @enderror"
+                                                                  placeholder="Direccion exacta"
+                                                                  required
+                                                                  maxlength="80" name="direccion">{{old("direccion")}}</textarea>
+                                                        @error('direccion')
+                                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Ingrese el télefono:</label>
-                                                <input class="form-control @error('telefono') is-invalid @enderror"
-                                                       placeholder="Télefono"
-                                                       required
-                                                       value="{{old("telefono")}}"
-                                                       maxlength="8"
-                                                       name="telefono">
-                                                @error('telefono')
-                                                <span class="invalid-feedback" role="alert">
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Ingrese el télefono:</label>
+                                                        <input class="form-control @error('telefono') is-invalid @enderror"
+                                                               placeholder="Télefono"
+                                                               required
+                                                               value="{{old("telefono")}}"
+                                                               maxlength="8"
+                                                               name="telefono">
+                                                        @error('telefono')
+                                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Ingrese el correo (opcional):</label>
-                                                <input class="form-control @error('email') is-invalid @enderror"
-                                                       placeholder="Correo Electronico"
-                                                       type="email"
-                                                       value="{{old("email")}}"
-                                                       maxlength="100"
-                                                       name="email">
-                                                @error('email')
-                                                <span class="invalid-feedback" role="alert">
+                                                        @enderror
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Ingrese el correo (opcional):</label>
+                                                        <input class="form-control @error('email') is-invalid @enderror"
+                                                               placeholder="Correo Electronico"
+                                                               type="email"
+                                                               value="{{old("email")}}"
+                                                               maxlength="100"
+                                                               name="email">
+                                                        @error('email')
+                                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                                @enderror
+                                                        @enderror
+                                                    </div>
+                                                    <hr>
+                                                    <button id="btnRegister" type="submit" class="btn btn-success">Guardar</button>
+                                                    <a id="btnCancel" class="btn btn-primary btn-round"
+                                                       data-dismiss="modal">Cancelar</a>
+                                                </form>
                                             </div>
-                                            <hr>
-                                            <button id="btnRegister" type="submit" class="btn btn-success">Guardar</button>
-                                             <a id="btnCancel" class="btn btn-primary btn-round"
-                                                data-dismiss="modal">Cancelar</a>
-                                        </form>
+                                        </div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
