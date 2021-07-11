532bf8c7ce0322ac42059e8ff02225d50e42bb5c
waleskamolina <cm817194@gmail.com>
2021-07-11 12:03:49 -0600

    N# 53 - Error Vista Proveedores

--- resources/views/proveedores/editar_proveedor.blade.php
+++ resources/views/proveedores/editar_proveedor.blade.php
@@ -2,117 +2,96 @@
 @extends("servicios.mejora_vista")
 @section("content")
 
-    <div class="container register" id="detalle_form_prov">
-        <div class="row" id="detalle_form_prov">
-            <div class="col-md-3 register-left">
-                <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
-                <h1>SMARTEC</h1>
-                <a id="btn-cancelar" class="btn btn-primary btn-round"
-                   href="{{route("proveedores.index")}}">Cancelar</a>
-            </div>
-            <div class="col-md-9 register-right">
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                        <h1 class="register-heading">EDITAR PROVEEDOR</h1>
-                        <div class="row register-form">
-                            <div class="col-md-6">
+<div class="container register" id="detalle_form_prov">
+    <div class="row" id="detalle_form_prov">
+        <div class="col-md-3 register-left">
+            <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt="" />
+            <h1>SMARTEC</h1>
+            <a id="btn-cancelar" class="btn btn-primary btn-round" href="{{route("proveedores.index")}}">Cancelar</a>
+        </div>
+        <div class="col-md-9 register-right">
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <h1 class="register-heading">EDITAR PROVEEDOR</h1>
+                    <div class="row register-form">
+                        <div class="col-md-6">
 
-                                <form id="form_proveedores" enctype="multipart/form-data"
-                                      action="{{route("proveedor.update",["id"=>$proveedor->id])}}"
-                                      method="post">
-                                    @method("PUT")
-                                    @csrf
+                            <form id="form_proveedores" enctype="multipart/form-data"
+                                action="{{route("proveedor.update",["id"=>$proveedor->id])}}" method="post">
+                                @method("PUT")
+                                @csrf
 
-                                    <div class="form-group">
-                                        <label>Nombre:</label>
-                                        <input class="form-control  @error('nombre') is-invalid @enderror"
-                                               type="text"
-                                               pattern="[A-Za-z ]{2,20}"
-                                               required
-                                               maxlength="20"
-                                               @if(old("nombre"))
-                                               value="{{old("nombre")}}"
-                                               @else
-                                               value="{{$proveedor->nombre}}"
-                                               @endif
-                                               id="nombre"
-                                               name="nombre">
-                                        @error('nombre')
-                                        <span class="invalid-feedback" role="alert">
+                                <div class="form-group">
+                                    <label>Nombre:</label>
+                                    <input class="form-control  @error('nombre') is-invalid @enderror" type="text"
+                                        pattern="[A-Za-z ]{2,20}" required maxlength="20" @if(old("nombre"))
+                                        value="{{old("nombre")}}" @else value="{{$proveedor->nombre}}" @endif
+                                        id="nombre" name="nombre">
+                                    @error('nombre')
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
-                                    </div>
+                                    @enderror
+                                </div>
 
-                                    <div class="form-group">
-                                        <label>Descripción (opcional):</label>
-                                        <textarea class="form-control @error('descripcion') is-invalid @enderror"
-                                                  maxlength="40"
-                                                  name="descripcion">@if(old("descripcion")){{old("descripcion")}}
+                                <div class="form-group">
+                                    <label>Descripción (opcional):</label>
+                                    <textarea class="form-control @error('descripcion') is-invalid @enderror"
+                                        maxlength="40" name="descripcion">@if(old("descripcion")){{old("descripcion")}}
                                             @else{{$proveedor->descripcion}}@endif</textarea>
-                                        @error('descripcion')
-                                        <span class="invalid-feedback" role="alert">
+                                    @error('descripcion')
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
-                                    </div>
+                                    @enderror
+                                </div>
 
-                                    <div class="form-group">
-                                        <label>Dirección:</label>
-                                        <textarea class="form-control @error('direccion') is-invalid @enderror"
-                                                  required
-                                                  maxlength="40"
-                                                  name="direccion">@if(old("direccion")){{old("direccion")}}
+                                <div class="form-group">
+                                    <label>Dirección:</label>
+                                    <textarea class="form-control @error('direccion') is-invalid @enderror" required
+                                        maxlength="40" name="direccion">@if(old("direccion")){{old("direccion")}}
                                             @else{{$proveedor->direccion}}@endif</textarea>
-                                        @error('direccion')
-                                        <span class="invalid-feedback" role="alert">
+                                    @error('direccion')
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
-                                    </div>
+                                    @enderror
+                                </div>
 
-                                    <div class="form-group">
-                                        <label>Télefono:</label>
-                                        <input class="form-control @error('telefono') is-invalid @enderror"
-                                               type="tel"
-                                               pattern='\d{8}'
-                                               required
-                                               @if(old("direccion"))
-                                               value="{{old("telefono")}}"
-                                               @else
-                                               value="{{$proveedor->telefono}}"
-                                               @endif
-                                               maxlength="8"
-                                               name="telefono">
-                                        @error('telefono')
-                                        <span class="invalid-feedback" role="alert">
+                                <div class="form-group">
+                                    <label>Télefono:</label>
+                                    <input class="form-control @error('telefono') is-invalid @enderror" type="tel"
+                                        pattern='\d{8}' required @if(old("direccion")) value="{{old("telefono")}}" @else
+                                        value="{{$proveedor->telefono}}" @endif maxlength="8" name="telefono">
+                                    @error('telefono')
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
-                                    </div>
+                                    @enderror
+                                </div>
 
-                                    <div class="form-group">
-                                        <label>Correo electrónico:</label>
-                                        <input class="form-control @error('email') is-invalid @enderror"
-                                               type="email"
-                                               @if(old("email"))
-                                               value="{{old("email")}}"
-                                               @else
-                                               value="{{$proveedor->email}}"
-                                               @endif
-                                               maxlength="25"
-                                               required
-                                               name="email">
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
+                                <div class="form-group">
+                                    <label>Correo electrónico:</label>
+                                    <input class="form-control @error('email') is-invalid @enderror" type="email"
+                                        @if(old("email")) value="{{old("email")}}" @else value="{{$proveedor->email}}"
+                                        @endif maxlength="25" required name="email">
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
-                                    </div>
-                                    <hr>
-                                    <button id="btnRegister" type="submit" class="btn btn-success">Guardar</button>
-                                </form>
-                            </div>
+                                    @enderror
+                                </div>
+                                <hr>
+                                <button id="btnRegister" type="submit" class="btn btn-success">Guardar</button>
+                            </form>
                         </div>
-@endsection
+                    </div>
 
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+@endsection
--- resources/views/proveedores/registrar_proveedor.blade.php
+++ resources/views/proveedores/registrar_proveedor.blade.php
@@ -1,118 +1,99 @@
 @extends("layouts.formulario")
 @section("contenido")
-                <h1>SMARTEC</h1>
-                <a id="btn-cancelar" class="btn btn-primary btn-round"
-                   href="{{route("proveedores.index")}}">Cancelar</a>
-            </div>
-            <div class="col-md-9 register-right">
-                <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                        <h1 class="register-heading">REGISTRAR PROVEEDOR</h1>
-                        <div class="row register-form">
-                            <div class="col-md-6">
+<h1>SMARTEC</h1>
+<a id="btn-cancelar" class="btn btn-primary btn-round" href="{{route("proveedores.index")}}">Cancelar</a>
+</div>
+<div class="col-md-9 register-right">
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <h1 class="register-heading">REGISTRAR PROVEEDOR</h1>
+            <div class="row register-form">
+                <div class="col-md-6">
 
-                                <form id="form_proveedores" name="form_proveedores" enctype="multipart/form-data"
-                                      action="{{route("proveedor.store")}}"
-                                      method="post">
-                                    @csrf
+                    <form id="form_proveedores" name="form_proveedores" enctype="multipart/form-data"
+                        action="{{route("proveedor.store")}}" method="post">
+                        @csrf
 
-                                    <div class="form-group">
-                                        <label>Nombre:</label>
-                                        <input class="form-control  @error('nombre') is-invalid @enderror"
-                                               placeholder=""
-                                               required
-                                               value="{{old("nombre")}}"
-                                               name="nombre"
-                                               id="nombre">
-                                        @error('nombre')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label>Descripción (opcional):</label>
-                                        <textarea class="form-control @error('descripcion') is-invalid @enderror"
-                                                  pattern="[A-Za-z,.áéíóú ]{1,40}"
-                                                  title="Debe ingresar una descripción valido, ejemplo: Esto es una descripción."
-                                                  placeholder=""
-                                                  maxlength="40" name="descripcion">{{old("descripcion")}}</textarea>
-                                        @error('descripcion')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label>Dirección:</label>
-                                        <textarea class="form-control @error('direccion') is-invalid @enderror"
-                                                  pattern="[A-Za-z,.áéíóú ]{1,40}"
-                                                  title="Debe ingresar una dirección valido, ejemplo: Esto es una descripción."
-                                                  placeholder=""
-                                                  required
-                                                  maxlength="40" name="direccion">{{old("direccion")}}</textarea>
-                                        @error('direccion')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label>Télefono:</label>
-                                        <input class="form-control @error('telefono') is-invalid @enderror"
-                                               placeholder=""
-                                               required
-                                               maxlength="8"
-                                               value="{{old("telefono")}}"
-                                               name="telefono">
-                                        @error('telefono')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label>Correo electrónico:</label>
-                                        <input class="form-control @error('email') is-invalid @enderror"
-                                               placeholder=""
-                                               id="email"
-                                               type="email"
-                                               value="{{old("email")}}"
-                                               maxlength="25"
-                                               required
-                                               name="email">
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
-                                    </div>
-                                    <hr>
-                                    <button id="btnRegister" onclick="validar()" class="btn btn-success">Guardar
-                                    </button>
-                                </form>
-                            </div>
+                        <div class="form-group">
+                            <label>Nombre:</label>
+                            <input class="form-control  @error('nombre') is-invalid @enderror" placeholder="" required
+                                value="{{old("nombre")}}" name="nombre" id="nombre">
+                            @error('nombre')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
                         </div>
 
-                        <script>
-                            function validar() {
-                                let isValid = false;
-                                var v_figura = document.getElementById('nombre').value;
-                                const pattern = new RegExp("^[a-zA-Z ]*$");
-                                if (pattern.test(v_figura)) {
-                                } else {
-                                    toastr.options =
-                                        {
-                                            "closeButton": true,
-                                            "progressBar": true
-                                        }
-                                    toastr.error("El nombre no puede incluir números");
-                                    event.preventDefault();
-                                }
-                            }
-                        </script>
+                        <div class="form-group">
+                            <label>Descripción (opcional):</label>
+                            <textarea class="form-control @error('descripcion') is-invalid @enderror"
+                                pattern="[A-Za-z,.áéíóú ]{1,40}"
+                                title="Debe ingresar una descripción valido, ejemplo: Esto es una descripción."
+                                placeholder="" maxlength="40" name="descripcion">{{old("descripcion")}}</textarea>
+                            @error('descripcion')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label>Dirección:</label>
+                            <textarea class="form-control @error('direccion') is-invalid @enderror"
+                                pattern="[A-Za-z,.áéíóú ]{1,40}"
+                                title="Debe ingresar una dirección valido, ejemplo: Esto es una descripción."
+                                placeholder="" required maxlength="40" name="direccion">{{old("direccion")}}</textarea>
+                            @error('direccion')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label>Télefono:</label>
+                            <input class="form-control @error('telefono') is-invalid @enderror" placeholder="" required
+                                maxlength="8" value="{{old("telefono")}}" name="telefono">
+                            @error('telefono')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label>Correo electrónico:</label>
+                            <input class="form-control @error('email') is-invalid @enderror" placeholder="" id="email"
+                                type="email" value="{{old("email")}}" maxlength="25" required name="email">
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                        <hr>
+                        <button id="btnRegister" onclick="validar()" class="btn btn-success">Guardar
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    function validar() {
+        let isValid = false;
+        var v_figura = document.getElementById('nombre').value;
+        const pattern = new RegExp("^[a-zA-Z ]*$");
+        if (pattern.test(v_figura)) {} else {
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.error("El nombre no puede incluir números");
+            event.preventDefault();
+        }
+    }
+</script>
 @endsection
