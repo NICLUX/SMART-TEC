fb97af5e09d81d90fad54aae0136d445d9263e70
Harol Miranda <harol.miranda@unah.hn>
2021-07-27 11:18:39 -0600

    Solución al error de ProveedorFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ DatabaseSeeder extends Seeder
     public function run()
     {
         //\App\Models\User::factory(2)->create();
-        \App\Models\Proveedor::factory(45)->create();
-        \App\Models\Cliente::factory(45)->create();
+        //\App\Models\Proveedor::factory(45)->create();
+        //\App\Models\Cliente::factory(45)->create();
         $this->call(Tipo_userSeeder::class);
         $this->call(UserTableseeder::class);
 
